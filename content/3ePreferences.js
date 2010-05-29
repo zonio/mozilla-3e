eff519aa57bcd2b278f7fc8f43654695c3fc9b4c
Filip Zrůst <filip.zrust@zonio.net>
2010-05-29 20:32:54 +0200

    Sorry, modification not included :-)

--- content/3ePreferences.js
+++ content/3ePreferences.js
@@ -1,38 +1,51 @@
 Components.utils.import("resource://gre/modules/iteratorUtils.jsm");
 
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
 if ('undefined' === typeof Calendar3e) {
   var Calendar3e = {};
 }
 
-Calendar3e.Preferences = function createObject() {
+Calendar3e.Preferences = function () {
   this.initialize();
 }
+Calendar3e.Preferences.EEE_ENABLED_KEY = 'eee_enabled';
 var CalPrefs = Calendar3e.Preferences.prototype;
 /**
  * Initializes 3e calendar prefences.
  */
-CalPrefs.initialize = function initialize() {
+CalPrefs.initialize = function () {
+  var console = Cc["@mozilla.org/consoleservice;1"].getService(
+      Ci.nsIConsoleService
+    );
+  this.console = console;
+
   var mgr = Components.classes["@mozilla.org/messenger/account-manager;1"]
-                      .getService(Components.interfaces.nsIMsgAccountManager);
+                      .getService(Ci.nsIMsgAccountManager);
   mgr.addIncomingServerListener(this);
   this._accountsManager = mgr;
 
-  this._loadAccounts();
-  this._fillAccountTable();
+  this._accountsDidChange();
 }
-CalPrefs.destroy = function destroy() {
+CalPrefs.destroy = function () {
   this._accountsManager.removeIncomingServerListener(this);
   this._accountsManager = null;
 }
-CalPrefs.onServerLoaded = function at_onServerLoaded(aServer) {
+CalPrefs.onServerLoaded = function (server) {
+  this._accountsDidChange();
+}
+CalPrefs.onServerUnloaded = function (server) {
+}
+CalPrefs.onServerChanged = function (server) {}
+/**
+ * Loads remote accounts, creates properties according to them
+ * and fills the UI with them.
+ */
+CalPrefs._accountsDidChange = function () {
   this._loadAccounts();
-  this._fillAccountTable();
-},
-CalPrefs.onServerUnloaded = function at_onServerUnloaded(aServer) {
-  this._loadAccounts();
-  this._fillAccountTable();
-},
-CalPrefs.onServerChanged = function at_onServerChanged(aServer) {},
+  this._fillAccountsTable();
+}
 /**
  * Loads accounts currently registred in Thunderbird.
  */
@@ -40,11 +53,10 @@ CalPrefs._loadAccounts = function () {
   var mgr = this._accountsManager;
 
   var accounts = [
-    a for each (a in fixIterator(mgr.accounts,
-                                 Components.interfaces.nsIMsgAccount))
+    a for each (a in fixIterator(mgr.accounts, Ci.nsIMsgAccount))
   ];
   //XXX incomingServer server check due to 41133
-  accounts = accounts.filter(function fix(a) {
+  accounts = accounts.filter(function (a) {
       return a.incomingServer && (a.incomingServer.type != "nntp")
                               && (a.incomingServer.type != "none");
     });
@@ -60,19 +72,29 @@ CalPrefs._loadAccounts = function () {
   this._accounts = accounts;
 }
 /**
- * Fills the UI with loaded accounts.
+ * Fills the UI with loaded accounts' default identities..
+ *
+ * Also registers handling to catch changes when user enables
+ * or diaables 3e abbilities with particular identity.
  */
-CalPrefs._fillAccountTable = function() {
+CalPrefs._fillAccountsTable = function () {
   var tree = document.getElementById('cal3e-accounts-tree-children');
   while (tree.firstChild) {
     tree.removeChild(tree.firstChild);
   }
+  var calPrefs = this;
+  tree.addEventListener('change', function (evt) {
+      calPrefs._eeeEnabledDidChange(evt);
+    }, false);
   for each (var account in this._accounts) {
-    let server = account.incomingServer;
+    var identity = account.defaultIdentity;
+
     var accountName = document.createElement('treecell');
-    accountName.setAttribute('label', server.rootFolder.prettyName);
-    var accountEnabled = document.createElement('treecell');
-    accountEnabled.setAttribute('value', 'false');
+    accountName.setAttribute('value', identity.key);
+    accountName.setAttribute('label', identity.identityName);
+    var accountEnabled = document.createElement('treecell'),
+        enabled = identity.getBoolAttribute(this.constructor.EEE_ENABLED_KEY);
+    accountEnabled.setAttribute('value', enabled ? 'true' : 'false');
     var treerow = document.createElement('treerow');
     treerow.appendChild(accountName);
     treerow.appendChild(accountEnabled);
@@ -81,9 +103,29 @@ CalPrefs._fillAccountTable = function() {
     tree.appendChild(treeitem);
   }
 }
+/**
+ * Enables or disables 3e support for identity associated with
+ * event's target (checkbox cell in tree).
+ *
+ * @param {Event} evt 
+ * @private
+ */
+CalPrefs._eeeEnabledDidChange = function (evt) {
+  var enabledCell = evt.target;
+  if (('treecell' != enabledCell.tagName) &&
+      ('checkbox' != enabledCell.getAttribute('type'))) {
+    return;
+  }
+  var enabled = 'true' === enabledCell.getAttribute('value') ? true : false ;
+
+  var identityCell = enabledCell.previousSibling,
+      mgr = this._accountsManager,
+      identity = mgr.getIdentity(identityCell.getAttribute('value'));
+  identity.setBoolAttribute(this.constructor.EEE_ENABLED_KEY, enabled);
+}
 
 Calendar3e.Preferences.onLoad = function onLoad() {
-  new Calendar3e.Preferences();
+  var prefs = new Calendar3e.Preferences();
 }
 
 window.addEventListener('load', Calendar3e.Preferences.onLoad, false);
