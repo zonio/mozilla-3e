50af503e0311f4352f1d2228f28248b1ef4ecbb5
Filip Zrůst <filip.zrust@zonio.net>
2012-07-10 14:25:57 +0200

    Rewarped calendar creation files
    Implementation stays the same but it's changed up to standards we want
    to achieve.  Which means more encapsulated code moved towards more
    functional style.
    
    The only change is a fix of synchronization between Lightning and our
    extension's identity controls.

--- chrome/content/calendarCreation.js
+++ chrome/content/calendarCreation.js
@@ -1,6 +1,6 @@
 /* ***** BEGIN LICENSE BLOCK *****
  * 3e Calendar
- * Copyright © 2011  Zonio s.r.o.
+ * Copyright © 2012  Zonio s.r.o.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,251 +17,267 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
+Components.utils.import("resource://calendar/modules/calUtils.jsm");
 Components.utils.import("resource://calendar3e/modules/cal3eUtils.jsm");
 
-var cal3eCreation = {};
+function cal3eCreation() {
+  var originalUri = "";
+  var uriComputed = false;
+  var ltn_initCustomizePage;
 
-/**
- * Checks value of selected calendar format and modifies dialog
- * accordingly.
- *
- * If calendar format is 3e, then URI textbox is hidden because URI is
- * computed automatically. Previous value is stored in case user
- * changes her mind.
- */
-cal3eCreation.selectionChanged = function selectionChanged() {
-  var calendarFormat = document.getElementById('calendar-format');
-  if ('eee' == calendarFormat.value) {
-    cal3eCreation._activate3eContext();
-  } else {
-    cal3eCreation._deactivate3eContext();
-  }
-};
+  /**
+   * Activates context for creation of 3e calendar.
+   *
+   * Activation is done by hiding row with calendar location textbox and
+   * by showing menulist to select 3e account which will be used for
+   * calendar creation.
+   */
+  function activate3eContext() {
+    var calendarUriElement = document.getElementById('calendar-uri');
 
-/**
- * Activates context for creation of 3e calendar.
- *
- * Activation is done by hiding row with calendar location textbox and
- * by showing menulist to select 3e account which will be used for
- * calendar creation.
- */
-cal3eCreation._activate3eContext = function activate3eContext() {
-  var calendarUri = document.getElementById('calendar-uri');
+    // hide URI textbox and store current value
+    originalUri = calendarUriElement.value || "";
+    calendarUriElement.parentNode.setAttribute('hidden', 'true');
+    computeUri();
 
-  // hide URI textbox and store current value
-  cal3eCreation._originalUri = calendarUri.value || "";
-  calendarUri.parentNode.setAttribute('hidden', 'true');
-  cal3eCreation.computeUri();
+    // hide lightning identity menu list if it is there
+    var identityRow = document.getElementById('calendar-email-identity-row');
+    if (null !== identityRow) {
+      identityRow.setAttribute('hidden', 'true');
+    }
 
-  // hide lightning identity menu list if it is there
-  var identityRow = document.getElementById('calendar-email-identity-row');
-  if (null !== identityRow) {
-    identityRow.setAttribute('hidden', 'true');
+    // fixes problem with setting calendar URI value for the first time
+    // when XUL doesn't dispatch command event properly
+    var commandEvent = document.createEvent('Event');
+    commandEvent.initEvent('command', true, true);
+    calendarUriElement.dispatchEvent(commandEvent);
+
+    // show account menulist with 3e-enabled accounts
+    load3eAccounts();
+    var calendar3eRow = document.getElementById('calendar3e-account-row');
+    calendar3eRow.setAttribute('hidden', 'false');
   }
 
-  // fixes problem with setting calendar URI value for the first time
-  // when XUL doesn't dispatch command event properly
-  var commandEvent = document.createEvent('Event');
-  commandEvent.initEvent('command', true, true);
-  calendarUri.dispatchEvent(commandEvent);
+  /**
+   * Loads fixtures of 3e accounts to accounts menu.
+   *
+   * @todo account loading this should be united for calendar
+   * preferences, calendar creation and other dialogs in some util
+   * function or found elsewhere
+   */
+  function load3eAccounts() {
+    var calendar3eAccounts = document.getElementById('calendar3e-account'),
+    menuPopup = calendar3eAccounts.firstChild;
+    var selectedIdentity = calendar3eAccounts.value;
+    while (menuPopup.lastChild) {
+      menuPopup.removeChild(menuPopup.lastChild);
+    }
 
-  // show account menulist with 3e-enabled accounts
-  cal3eCreation._load3eAccounts();
-  var calendar3eRow = document.getElementById('calendar3e-account-row');
-  calendar3eRow.setAttribute('hidden', 'false');
-};
+    var accountCollection = new cal3e.AccountCollection();
+    var accounts = accountCollection.filter(
+      cal3e.AccountCollection.filterEnabled
+    );
+    var idx = accounts.length;
+    var account, identity, item;
+    while (idx--) {
+      account = accounts[idx];
+      identity = account.defaultIdentity;
+      item = calendar3eAccounts.appendItem(
+        identity.fullName + " <" + identity.email + ">",
+        identity.key);
 
-/**
- * Loads fixtures of 3e accounts to accounts menu.
- *
- * @todo account loading this should be united for calendar
- * preferences, calendar creation and other dialogs in some util
- * function or found elsewhere
- */
-cal3eCreation._load3eAccounts = function load3eAccounts() {
-  var calendar3eAccounts = document.getElementById('calendar3e-account'),
-      menuPopup = calendar3eAccounts.firstChild;
-  var selectedIdentity = calendar3eAccounts.value;
-  while (menuPopup.lastChild) {
-    menuPopup.removeChild(menuPopup.lastChild);
-  }
-
-  var accountCollection = new cal3e.AccountCollection();
-  var accounts = accountCollection.filter(
-        cal3e.AccountCollection.filterEnabled),
-      idx = accounts.length, account, identity, item;
-  while (idx--) {
-    account = accounts[idx];
-    identity = account.defaultIdentity;
-    item = calendar3eAccounts.appendItem(
-      identity.fullName + " <" + identity.email + ">",
-      identity.key);
-
-    if (identity.key == selectedIdentity) {
-      calendar3eAccounts.selectedItem = item;
+      if (identity.key == selectedIdentity) {
+        calendar3eAccounts.selectedItem = item;
+      }
     }
   }
-};
 
-/**
- * Deactivates context for creation of 3e calendar.
- *
- * Deactivation is done by hiding menulist to select 3e account which
- * will be used for calendar creation and by showing row with calendar
- * location textbox.
- */
-cal3eCreation._deactivate3eContext = function deactivate3eContext() {
-  // hide account menulist
-  var accountRow = document.getElementById('calendar3e-account-row');
-  accountRow.setAttribute('hidden', 'true');
+  /**
+   * Deactivates context for creation of 3e calendar.
+   *
+   * Deactivation is done by hiding menulist to select 3e account which
+   * will be used for calendar creation and by showing row with calendar
+   * location textbox.
+   */
+  function deactivate3eContext() {
+    // hide account menulist
+    var accountRow = document.getElementById('calendar3e-account-row');
+    accountRow.setAttribute('hidden', 'true');
 
-  // hide lightning identity menu list if it is there
-  var identityRow = document.getElementById('calendar-email-identity-row');
-  if (null !== identityRow) {
-    identityRow.setAttribute('hidden', 'false');
+    // hide lightning identity menu list if it is there
+    var identityRow = document.getElementById('calendar-email-identity-row');
+    if (null !== identityRow) {
+      identityRow.setAttribute('hidden', 'false');
+    }
+
+    // show location textbox with previously entered URI
+    var calendarUriElement = document.getElementById('calendar-uri');
+    if ('undefined' !== typeof originalUri) {
+      calendarUriElement.value = originalUri;
+    } else {
+      calendarUriElement.value = calendarUriElement.value || "";
+    }
+    calendarUriElement.parentNode.setAttribute('hidden', 'false');
   }
 
-  // show location textbox with previously entered URI
-  var calendarUri = document.getElementById('calendar-uri');
-  if ('undefined' !== typeof cal3eCreation._originalUri) {
-    calendarUri.value = cal3eCreation._originalUri;
-  } else {
-    calendarUri.value = calendarUri.value || "";
-  }
-  calendarUri.parentNode.setAttribute('hidden', 'false');
-};
-
-/**
- * Sets EEE URI to location textbox according to currently available
- * information about created 3e calendar.
- *
- * Does nothing if created calendar isn't 3e calendar.
- */
-cal3eCreation.computeUri = function computeUri() {
-  var calendarFormat = document.getElementById('calendar-format');
-  if ('eee' != calendarFormat.value) {
-    return;
-  }
-  if (cal3eCreation._uriComputed) {
-    return;
-  }
-
-  var account = document.getElementById('calendar3e-account'),
-      uri = "eee://";
-
-  var accountCollection = new cal3e.AccountCollection();
-  var accounts = accountCollection.filter(
-        cal3e.AccountCollection.filterEnabled),
-      idx = accounts.length;
-  var found = false
-  while (idx--) {
-    if (accounts[idx].defaultIdentity.key == account.value) {
-      uri += accounts[idx].defaultIdentity.email;
-      found = true;
-      break;
+  /**
+   * Checks value of selected calendar format and modifies dialog
+   * accordingly.
+   *
+   * If calendar format is 3e, then URI textbox is hidden because URI
+   * is computed automatically. Previous value is stored in case user
+   * changes her mind.
+   */
+  function selectionChanged() {
+    var calendarFormat = document.getElementById('calendar-format');
+    if ('eee' == calendarFormat.value) {
+      activate3eContext();
+    } else {
+      deactivate3eContext();
     }
   }
-  uri += '/';
 
-  if (found) {
-    cal3eCreation._uriComputed = true;
+  /**
+   * Dynamically creates elements necessary for 3e calendar creation.
+   *
+   * Ovelay isn't used in this situation because calendar creation
+   * wizard makes it hard to provide custom UI on the page where
+   * calendar type and location are edited.
+   *
+   * Menulist and its label along with a row where they will reside are
+   * created and inserted on the page mentioned before. Row is
+   * identified as 'calendar3e-account-row' and menulist as
+   * 'calendar3e-account'.
+   */
+  function overlay() {
+    var accountMenuList = document.createElementNS(
+      "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+      'menulist'
+    );
+    accountMenuList.id = 'calendar3e-account';
+    accountMenuList.flex = 1;
+    accountMenuList.appendChild(document.createElementNS(
+      "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+      'menupopup'
+    ));
+
+    var accountLabel = document.createElementNS(
+      "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+      'label'
+    );
+    accountLabel.control = 'calendar3e-account';
+
+    var accountRow = document.createElementNS(
+      "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul",
+      'row'
+    );
+    accountRow.id = 'calendar3e-account-row';
+    accountRow.align = 'center';
+    accountRow.insertBefore(accountMenuList, null);
+    accountRow.insertBefore(accountLabel, accountMenuList);
+
+    var notifications = document.getElementById('location-notifications');
+    notifications.parentNode.insertBefore(accountRow, notifications);
+
+    // doesn't work when set before actually being in the document
+    var stringBundle = document.getElementById('calendar3e-strings');
+    accountLabel.value = stringBundle.getString(
+      'cal3eCalendarProperties.account.label'
+    );
+
+    accountMenuList.addEventListener('command', computeUri, false);
+
+    var nameTextbox = document.getElementById('calendar-name');
+    nameTextbox.addEventListener('input', computeUri, false);
+
+    // sadly, this is better solution but cannot there's no way to make
+    // event handler hadle page show in the right time (after
+    // Lightning's page initialization)
+    //var calendarWizard = document.getElementById('calendar-wizard');
+    //calendarWizard.getPageById('customizePage').addEventListener(
+    //  'pageshow', cal3eCreation.syncIdentity, false);
+    // ... so we have to override that Lightning's page initialization
+    ltn_initCustomizePage = initCustomizePage;
+    initCustomizePage = function cal3e_initCustomizePage() {
+      ltn_initCustomizePage();
+      syncIdentity();
+    };
   }
 
-  var calendarUri = document.getElementById('calendar-uri');
-  calendarUri.value = uri;
+  /**
+   * Sets EEE URI to location textbox according to currently available
+   * information about created 3e calendar.
+   *
+   * Does nothing if created calendar isn't 3e calendar.
+   */
+  function computeUri() {
+    var calendarFormat = document.getElementById('calendar-format');
+    if ('eee' != calendarFormat.value) {
+      return;
+    }
+    if (uriComputed) {
+      return;
+    }
 
-  if (gCalendar) {
-    var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-      .getService(Components.interfaces.nsIIOService);
-    gCalendar.uri = ioService.newURI(uri, null, null);
-  }
-};
+    var account = document.getElementById('calendar3e-account'),
+    uri = "eee://";
 
-/**
- * Synces identity set on 3e overlay controls to Lightning overlay
- * controls.
- *
- * This ensures that calendar gets proper identity set.
- */
-cal3eCreation.syncIdentity = function syncIdentity() {
-  var cal3eIdentity = document.getElementById('calendar3e-account');
-  var lightningIdentity = document.getElementById('email-identity-menulist');
+    var accountCollection = new cal3e.AccountCollection();
+    var accounts = accountCollection.filter(
+      cal3e.AccountCollection.filterEnabled
+    );
+    var idx = accounts.length;
+    var found = false
+    while (idx--) {
+      if (accounts[idx].defaultIdentity.key == account.value) {
+        uri += accounts[idx].defaultIdentity.email;
+        found = true;
+        break;
+      }
+    }
+    uri += '/';
 
-  var idx = lightningIdentity.itemCount, item;
-  while (idx--) {
-    item = lightningIdentity.getItemAtIndex(idx);
-    if (item.value == cal3eIdentity.value) {
-      lightningIdentity.selectedItem = item;
-      break;
+    if (found) {
+      uriComputed = true;
+    }
+
+    document.getElementById('calendar-uri').value = uri;
+
+    if (gCalendar) {
+      var ioService = Components.classes["@mozilla.org/network/io-service;1"].
+        getService(Components.interfaces.nsIIOService);
+      gCalendar.uri = ioService.newURI(uri, null, null);
     }
   }
-};
 
-/**
- * Dynamically creates elements necessary for 3e calendar creation.
- *
- * Ovelay isn't used in this situation because calendar creation
- * wizard makes it hard to provide custom UI on the page where
- * calendar type and location are edited.
- *
- * Menulist and its label along with a row where they will reside are
- * created and inserted on the page mentioned before. Row is
- * identified as 'calendar3e-account-row' and menulist as
- * 'calendar3e-account'.
- */
-cal3eCreation.overlay = function overlay() {
-  var accountMenuList = document.createElement('menulist');
-  accountMenuList.id = 'calendar3e-account';
-  accountMenuList.flex = 1;
-  accountMenuList.appendChild(document.createElement('menupopup'));
+  /**
+   * Synces identity set on 3e overlay controls to Lightning overlay
+   * controls.
+   *
+   * This ensures that calendar gets proper identity set.
+   */
+  function syncIdentity() {
+    var cal3eIdentity = document.getElementById('calendar3e-account');
+    var lightningIdentity = document.getElementById('email-identity-menulist');
 
-  var accountLabel = document.createElement('label');
-  accountLabel.control = 'calendar3e-account';
+    var idx = lightningIdentity.itemCount;
+    while (idx--) {
+      if (lightningIdentity.getItemAtIndex(idx).getAttribute('value') ==
+          cal3eIdentity.value) {
+        lightningIdentity.selectedItem = lightningIdentity.getItemAtIndex(idx);
+        break;
+      }
+    }
+  }
 
-  var accountRow = document.createElement('row');
-  accountRow.id = 'calendar3e-account-row';
-  accountRow.align = 'center';
-  accountRow.insertBefore(accountMenuList, null);
-  accountRow.insertBefore(accountLabel, accountMenuList);
+  overlay();
+  document.getElementById('calendar-format').addEventListener(
+    'command', selectionChanged, false
+  );
+  selectionChanged();
+}
 
-  var notifications = document.getElementById('location-notifications');
-  notifications.parentNode.insertBefore(accountRow, notifications);
-
-  // doesn't work when set before actually being in the document
-  var stringBundle = document.getElementById('calendar3e-strings');
-  accountLabel.value = stringBundle.getString(
-    'cal3eCalendarProperties.account.label');
-
-  accountMenuList.addEventListener('command', cal3eCreation.computeUri, false);
-
-  var nameTextbox = document.getElementById('calendar-name');
-  nameTextbox.addEventListener('input', cal3eCreation.computeUri, false);
-
-  // sadly, this is better solution but cannot there's no way to make
-  // event handler hadle page show in the right time (after
-  // Lightning's page initialization)
-  //var calendarWizard = document.getElementById('calendar-wizard');
-  //calendarWizard.getPageById('customizePage').addEventListener(
-  //  'pageshow', cal3eCreation.syncIdentity, false);
-  // ... so we have to override that Lightning's page initialization
-  cal3eCreation._ltn_initCustomizePage = initCustomizePage;
-  initCustomizePage = function cal3e_initCustomizePage() {
-    cal3eCreation._ltn_initCustomizePage();
-    cal3eCreation.syncIdentity();
-  };
-};
-
-/**
- * Initializes calendar creation dialog with 3e extesion specific
- * behavior.
- */
-cal3eCreation.init = function init() {
-  cal3eCreation.overlay();
-
-  var calendarFormat = document.getElementById('calendar-format');
-  calendarFormat.addEventListener(
-    'command', cal3eCreation.selectionChanged, false);
-  cal3eCreation.selectionChanged();
-};
-
-window.addEventListener('load', cal3eCreation.init, false);
+cal3eCreation.onLoad = function () {
+  var controller = new cal3eCreation();
+}
+window.addEventListener('load', cal3eCreation.onLoad, false);
