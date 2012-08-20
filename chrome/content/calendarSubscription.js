be2f5787567f971349c2cda8cbd65c1a5a57062d
Filip Zrůst <filip.zrust@zonio.net>
2012-08-20 15:06:52 +0200

    Cleanup and style change of subscriptions
    Coding style is in line with newer view controller (e.g., calendar
    creation).  Localization entities were fixed.

--- chrome/content/calendarSubscription.js
+++ chrome/content/calendarSubscription.js
@@ -17,192 +17,91 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-Components.utils.import("resource://calendar3e/modules/identity.jsm");
-Components.utils.import("resource://calendar3e/modules/request.jsm");
-Components.utils.import("resource://calendar3e/modules/utils.jsm");
+Components.utils.import('resource://calendar3e/modules/identity.jsm');
+Components.utils.import('resource://calendar3e/modules/request.jsm');
+Components.utils.import('resource://calendar3e/modules/utils.jsm');
 
-function calendarSubscription() {
-  this._identityObserver = cal3eIdentity.Observer();
-  this._identityObserver.addObserver(this.onIdentityChange.bind(this));
-  this._accountManager = Components.classes[
-    "@mozilla.org/messenger/account-manager;1"
-  ].getService(Components.interfaces.nsIMsgAccountManager);
-  this._stringBundle = document.getElementById('calendar3e-strings');
-  this._subscriberElement = document.getElementById('subscriber-menulist');
-  this._providerMap = {};
-  this._providerToTreeItemMap = {};
+function cal3eSubscription() {
+  var cal3eSubscription = this;
+  var identityObserver;
+  var accountManager;
+  var stringBundle;
+  var subscriberElement;
 
-  this.onIdentityChange();
-  this.load();
-}
+  function subscribe() {
+  }
 
-calendarSubscription.LOADING = 0x0100;
-calendarSubscription.BROWSING = 0x0200;
-calendarSubscription.ERROR = 0xfe00;
+  function onIdentityChange() {
+    clearMenu(subscriberElement);
 
-calendarSubscription.prototype = {
-
-  getIdentity: function calendarSubscription_getIdentity() {
-    return this._subscriberElement.value ?
-      this._accountManager.getIdentity(this._subscriberElement.value) :
-      null ;
-  },
-
-  load: function calendarSubscription_load() {
-    this._state = calendarSubscription.LOADING;
-    this.loadProviders();
-    this.loadCalendars();
-  },
-
-  browse: function calendarSubscription_browse() {
-    this._state = calendarSubscription.BROWSING;
-  },
-
-  error: function calendarSubscription_error() {
-    this._state = calendarSubscription.ERROR;
-  },
-
-  loadProviders: function calendarSubscription_loadProviders() {
-    var identity = this.getIdentity();
-    if (null === identity) {
-      this._addItemToMenu(
-        this._providerElement,
-        [this._stringBundle.getString(
-          'cal3eCalendarProperties.providers.selectContextLabel'),
-         null],
-        true);
-      return;
-    }
-
-    this._addItemToMenu(
-      this._providerElement,
-      [this._stringBundle.getString(
-        'cal3eCalendarProperties.providers.loadingLabel'),
-       null],
-      true);
-    var calendarSubscription = this;
-    var listener = function calEee_adoptItem_onResult(methodQueue, result) {
-      if (!(result instanceof cal3eResponse.Success)) {
-        this._addItemToMenu(
-          this._providerElement,
-          [
-            this._stringBundle.getString(
-              'cal3eCalendarProperties.providers.errorLabel'
-            ),
-            null
-          ],
-          true);
-        return;
-      }
-      calendarSubscription.onProvidersLoaded(
-        calendarSubscription._buildProviders(result)
-      );
-    };
-    cal3eRequest.Client.getInstance().getUsers(
-      identity, listener,
-      "NOT match_username(" + identity.email + ") AND " +
-        "NOT match_user_alias(" + identity.email + ")"
-    );
-  },
-
-  loadCalendar: function calendarSubscription_loadCalendar() {
-  },
-
-  onIdentityChange: function calendarSubscription_onIdentityChange() {
-    this._clearMenu(this._subscriberElement);
-
-    cal3eIdentity.Collection().
-      getEnabled().
-      forEach(function(identity) {
-        var item = this._subscriberElement.appendItem(
-          identity.fullName + " <" + identity.email + ">",
+    cal3eIdentity.Collection()
+      .getEnabled()
+      .forEach(function(identity) {
+        var item = subscriberElement.appendItem(
+          identity.fullName + ' <' + identity.email + '>',
           identity.key
         );
 
-        if (identity.key == this._subscriberElement.value) {
-          this._subscriberElement.selectedItem = item;
+        if (identity.key == subscriberElement.value) {
+          subscriberElement.selectedItem = item;
         }
       });
-  },
+  }
 
-  onProvidersLoaded:
-  function calendarSubscription_onProvidersLoaded(providers) {
-    this._clearMenu(this._providerElement);
-    providers.forEach(function (provider) {
-      this._addItemToTree(this._providerElement, provider, false);
-    }, this);
-  },
+  function loadProviders() {
+    var identity = getIdentity();
+    if (null === identity) {
+      addItemToMenu(
+        providerElement,
+        [stringBundle.getString(
+           'cal3eCalendarProperties.providers.selectContextLabel'
+         ), null],
+        true
+      );
+      return;
+    }
 
-  _addItemToMenu:
-  function calendarSubscription_addItemToMenu(menu, item, clear) {
-    if ('menupopup' != menu.tagName) {
-      menu = menu.firstChild;
-      while (menu && ('menupopup' !== menu.tagName)) {
-        menu = menu.nextSibling;
+    addItemToMenu(
+      providerElement,
+      [stringBundle.getString(
+         'cal3eCalendarProperties.providers.loadingLabel'
+       ), null],
+      true
+    );
+    var listener = function calEee_adoptItem_onResult(methodQueue, result) {
+      if (!(result instanceof cal3eResponse.Success)) {
+        addItemToMenu(
+          providerElement,
+          [stringBundle.getString(
+             'cal3eCalendarProperties.providers.errorLabel'
+           ), null],
+          true
+        );
+        return;
       }
-    }
-    if (!menu || ('menupopup' != menu.tagName)) {
-      throw Components.Exception("Cannot find menupopup.");
-    }
-    if (clear) {
-      this._clearMenu(menu);
-    }
-    menu = menu.parentNode.appendItem.apply(menu.parentNode, item);
-  },
+      onProvidersLoaded(buildProviders(result));
+    };
+    cal3eRequest.Client.getInstance().getUsers(
+      identity, listener,
+      "NOT match_username('" + identity.email + "') AND " +
+        "NOT match_user_alias('" + identity.email + "')"
+    );
+  }
 
-  _clearMenu:
-  function calendarSubscription_clearMenu(menu) {
-    if ('menupopup' != menu.tagName) {
-      menu = menu.firstChild;
-      while (menu && ('menupopup' !== menu.tagName)) {
-        menu = menu.nextSibling;
-      }
-    }
-    if (!menu || ('menupopup' != menu.tagName)) {
-      throw Components.Exception("Cannot find menupopup.");
-    }
-    while (menu.lastChild) {
-      menu.removeChild(menu.lastChild);
-    }
-  },
-
-  _addItemToTree:
-  function calendarSubscription_addItemToTree(tree, item, parent) {
-    if ('treechildren' != tree.tagName) {
-      tree = tree.firstChild;
-      while (tree && ('treechildren' !== tree.tagName)) {
-        tree = tree.nextSibling;
-      }
-    }
-    if (!tree || ('treechildren' != tree.tagName)) {
-      throw Components.Exception("Cannot find treechildren.");
-    }
-  },
-
-  _clearTree:
-  function calendarSubscription_clearTree(tree) {
-    if ('treechildren' != tree.tagName) {
-      tree = tree.firstChild;
-      while (tree && ('treechildren' !== tree.tagName)) {
-        tree = tree.nextSibling;
-      }
-    }
-    if (!tree || ('treechildren' != tree.tagName)) {
-      throw Components.Exception("Cannot find treechildren.");
-    }
-    while (tree.lastChild) {
-      tree.removeChild(tree.lastChild);
-    }
-  },
-
-  _buildProviders:
-  function calendarSubscription_buildProviders(result) {
-    return result.data.map(function(rawProvider) {
-      return this._buildProvider(rawProvider);
+  function onProvidersLoaded(providers) {
+    clearMenu(providerElement);
+    providers.forEach(function(provider) {
+      addItemToTree(providerElement, provider, false);
     });
-  },
+  }
 
-  _buildProvider: function calendarSubscription_buildProvider(rawProvider) {
+  function buildProviders(result) {
+    return result.data.map(function(rawProvider) {
+      return buildProvider(rawProvider);
+    });
+  }
+
+  function buildProvider(rawProvider) {
     var provider = [];
     var username = rawProvider['username'];
     var realname;
@@ -215,37 +114,113 @@ calendarSubscription.prototype = {
       }
     }
     if (realname) {
-      provider.push(realname + "<" + username + ">");
+      provider.push(realname + ' <' + username + '>');
     } else {
       provider.push(username);
     }
     provider.push(username);
 
     return provider;
-  },
-
-  finalize: function calendarSubscription_finalize() {
-    this._identityObserver.destroy();
-    this._identityObserver = null;
   }
 
+  function addItemToMenu(menu, item, clear) {
+    if ('menupopup' != menu.tagName) {
+      menu = menu.firstChild;
+      while (menu && ('menupopup' !== menu.tagName)) {
+        menu = menu.nextSibling;
+      }
+    }
+    if (!menu || ('menupopup' != menu.tagName)) {
+      throw Components.Exception('Cannot find menupopup.');
+    }
+    if (clear) {
+      clearMenu(menu);
+    }
+    menu = menu.parentNode.appendItem.apply(menu.parentNode, item);
+  }
+
+  function clearMenu(menu) {
+    if ('menupopup' != menu.tagName) {
+      menu = menu.firstChild;
+      while (menu && ('menupopup' !== menu.tagName)) {
+        menu = menu.nextSibling;
+      }
+    }
+    if (!menu || ('menupopup' != menu.tagName)) {
+      throw Components.Exception('Cannot find menupopup.');
+    }
+    while (menu.lastChild) {
+      menu.removeChild(menu.lastChild);
+    }
+  }
+
+  function addItemToTree(tree, item, parent) {
+    if ('treechildren' != tree.tagName) {
+      tree = tree.firstChild;
+      while (tree && ('treechildren' !== tree.tagName)) {
+        tree = tree.nextSibling;
+      }
+    }
+    if (!tree || ('treechildren' != tree.tagName)) {
+      throw Components.Exception('Cannot find treechildren.');
+    }
+    //TODO append under parent
+  }
+
+  function clearTree(tree) {
+    if ('treechildren' != tree.tagName) {
+      tree = tree.firstChild;
+      while (tree && ('treechildren' !== tree.tagName)) {
+        tree = tree.nextSibling;
+      }
+    }
+    if (!tree || ('treechildren' != tree.tagName)) {
+      throw Components.Exception('Cannot find treechildren.');
+    }
+    while (tree.lastChild) {
+      tree.removeChild(tree.lastChild);
+    }
+  }
+
+  function getIdentity() {
+    return subscriberElement.value ?
+      accountManager.getIdentity(subscriberElement.value) :
+      null;
+  }
+
+  function init() {
+    identityObserver = cal3eIdentity.Observer();
+    identityObserver.addObserver(onIdentityChange);
+    accountManager = Components.classes[
+      '@mozilla.org/messenger/account-manager;1'
+    ].getService(Components.interfaces.nsIMsgAccountManager);
+    stringBundle = document.getElementById('calendar3e-strings');
+    subscriberElement = document.getElementById('subscriber-menulist');
+
+    window.addEventListener('unload', finalize, false);
+    window.addEventListener('dialogaccept', subscribe, false);
+
+    onIdentityChange();
+    loadProviders();
+  }
+
+  function finalize() {
+    identityObserver.destroy();
+    identityObserver = null;
+  }
+
+  init();
 }
 
+cal3eSubscription.open = function cal3eSubscription_open() {
+  openDialog(
+    'chrome://calendar3e/content/calendarSubscription.xul',
+    'cal3eSubscription',
+    'chrome,titlebar,modal,resizable'
+  );
+};
+cal3eSubscription.onLoad = function cal3eSubscription_onLoad() {
+  new cal3eSubscription();
+};
 
-var subscribeDialog;
-calendarSubscription.open = function () {
-  openDialog("chrome://calendar3e/content/calendarSubscription.xul",
-             "cal3eSubscription", "chrome,titlebar,modal,resizable");
-}
-calendarSubscription.onLoad = function () {
-  subscribeDialog = new calendarSubscription();
-}
-calendarSubscription.onAccept = function () {
-  subscribeDialog.store();
-
-  return true;
-}
-calendarSubscription.onUnload = function () {
-  subscribeDialog.finalize();
-  subscribeDialog = null;
-}
+window.addEventListener('load', cal3eSubscription.onLoad, false);
