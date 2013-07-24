d4806507700136da10d31bbe2a185e72899849f0
Jan Šidlo <jan.sidlo@zonio.net>
2013-07-24 12:12:19 +0200

    Delete old set permissions code
    It was never fully functional and setting permission
    will be moved to different tab so it must be rewritten.

--- chrome/content/properties.js
+++ chrome/content/properties.js
@@ -26,30 +26,6 @@ Components.utils.import("resource://calendar3e/modules/utils.jsm");
 var cal3eProperties = {};
 
 /**
- * Shows permissions control if calendar type is shared.
- *
- * Otherwise ensures that permissions control is hidden.
- */
-cal3eProperties.typeChange = function typeChanged() {
-  if ('eee' != cal3eProperties._calendar.type) {
-    return;
-  }
-
-  var calendarEeeType = document.getElementById('calendar3e-type-group'),
-      permissionsRow = document.getElementById('calendar3e-permissions-row');
-  if ('shared' == calendarEeeType.selectedItem.value) {
-    cal3eProperties._loadUsers();
-    if (cal3eFeature.isSupported('permissions')) {
-      permissionsRow.removeAttribute('hidden');
-    }
-  } else {
-    if (cal3eFeature.isSupported('permissions')) {
-      permissionsRow.hidden = 'true';
-    }
-  }
-};
-
-/**
  * Hides 3e controls introduced by overlay and shows those which
  * overlay hides.
  */
@@ -63,112 +39,14 @@ cal3eProperties.hide3eControls = function hide3eControls() {
   var readOnlyRow = document.getElementById('calendar-readOnly-row');
   readOnlyRow.removeAttribute('hidden');
 
-  if (cal3eFeature.isSupported('permissions')) {
-    var typeRow = document.getElementById('calendar3e-type-row');
-    typeRow.hidden = 'true';
-    var permissionsRow = document.getElementById(
-      'calendar3e-permissions-row'
-    );
-    permissionsRow.hidden = 'true';
-  }
-
   if (cal3eProperties._init) {
     window.sizeToContent();
   }
 };
 
 /**
- * Loads users from server.
- */
-cal3eProperties._loadUsers = function loadUsers() {
-  var permissionsListBox = document.getElementById('permissions');
-  var permissionsListCols = permissionsListBox.getElementsByTagName(
-    "listcols")[0];
-  while (permissionsListCols.nextSibling) {
-    permissionsListBox.removeChild(permissionsListCols.nextSibling);
-  }
-
-  var clientListener = function cal3eProperties_loadUsers_onResult(result) {
-    if (!(result instanceof cal3eResponse.Success)) {
-      //TODO can't get list of users
-      return;
-    }
-
-    result.data.forEach(function(user) {
-      permissionsListBox.appendChild(
-        cal3eProperties._listItemFromUser(user)
-      );
-    });
-  };
-
-  var uriSpec = cal3eProperties._calendar.uri.spec,
-      uriParts = uriSpec.split('/', 4),
-      eeeUser = uriParts[2];
-  var accountManager = Cc["@mozilla.org/messenger/account-manager;1"].
-    getService(Components.interfaces.nsIMsgAccountManager);
-  var identities = [i for each (i in fixIterator(
-    accountManager.allIdentities, Components.interfaces.nsIMsgIdentity
-  ))];
-  var idx = identities.length;
-  var identity = null;
-  while (idx--) {
-    if (identities[idx].getBoolAttribute('eee_enabled') &&
-        (eeeUser == identities[idx].email)) {
-      identity = identities[idx];
-      break;
-    }
-  }
-  cal3eRequest.Client.getInstance().getUsers(identity, clientListener, "");
-};
-
-cal3eProperties._listItemFromUser = function listItemFromAccount(user) {
-  var listItem = document.createElement("listitem");
-  listItem.allowevents = 'true';
-
-  var nameListCell = document.createElement("listcell");
-  nameListCell.appendChild(document.createTextNode(
-    cal3eModel.userLabel(user))
-  );
-  listItem.appendChild(nameListCell);
-
-  var stringBundle = document.getElementById('calendar3e-strings');
-
-  var permissionListCell = document.createElement("listcell");
-  var permissionMenuList = document.createElement("menulist");
-  var permissionMenuPopup = document.createElement("menupopup");
-
-  permissionMenuList.label = stringBundle ?
-    stringBundle.getString('calendar3e.properties.permissions.label') :
-    "Permissions" ;
-
-  var readPermissionMenuItem = document.createElement("menuitem");
-  readPermissionMenuItem.setAttribute(
-    'label',
-    stringBundle ? stringBundle.getString('calendar3e.properties.read.label') : "Read Only");
-  readPermissionMenuItem.setAttribute(
-    'value',
-    'read');
-  permissionMenuPopup.appendChild(readPermissionMenuItem);
-
-  var writePermissionMenuItem = document.createElement("menuitem");
-  writePermissionMenuItem.setAttribute(
-    'label',
-    stringBundle ? stringBundle.getString('calendar3e.properties.write.label') : "Read/Write");
-  writePermissionMenuItem.setAttribute(
-    'value',
-    'write');
-  permissionMenuPopup.appendChild(writePermissionMenuItem);
-
-  permissionMenuList.appendChild(permissionMenuPopup);
-  permissionListCell.appendChild(permissionMenuList);
-  listItem.appendChild(permissionListCell);
-
-  return listItem;
-};
-
-/**
  * Takes content of "General Information" tab moves it back to dialog
- * and removes all tabs.
+ * and removes whole tab layout.
  */
 cal3eProperties.destroyTabs = function destroyTabs() {
   var tabbedVBox = document.getElementById('calendar3e-tabpanel-general-vbox');
